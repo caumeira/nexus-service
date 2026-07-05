60912c536eb52053acf7fa8524ff98144b696779
geretti <45468109+geretti@users.noreply.github.com>
2026-07-05 12:14:38 -0700

    feat(system): add /system/boot endpoint for once-per-boot UI greeting

--- src/Serialization/AppJsonContext.cs
+++ src/Serialization/AppJsonContext.cs
@@ -657,6 +657,7 @@ namespace Nexus.Service.Serialization;
 [JsonSerializable(typeof(SetVolumeBody))]
 [JsonSerializable(typeof(SetMutedBody))]
 [JsonSerializable(typeof(SystemAccentResponse))]
+[JsonSerializable(typeof(SystemBootResponse))]
 [JsonSerializable(typeof(SendKeysBody))]
 [JsonSerializable(typeof(SendTextBody))]
 [JsonSerializable(typeof(OpenPathBody))]
